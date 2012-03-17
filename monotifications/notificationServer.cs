d40a927fddb63e2e40a594eef0b4bb40c6a1db1d
Tomer Cohen <tomerc+github.com@gmail.com>
2012-03-17 02:02:31 +0200

    monotifications/notificationServer.cs

---
+++ monotifications/notificationServer.cs
@@ -0,0 +1,75 @@
+using System;
+using monotifications;
+
+namespace notificationConsoleServer
+{
+	public class NotificationServer : monotifications.notificationClient
+	{
+		protected Configuration machines;
+		
+		public NotificationServer () : this("server.ini")
+		{
+		}
+		
+		public NotificationServer (string serverINI) : this(serverINI, 7778)
+		{
+		}
+		
+		public NotificationServer (string serverINI, int listenPort) : base(serverINI, listenPort)
+		{
+		}
+		
+		public NotificationServer (string serverINI, int listenPort, string machinesINI)
+		{
+			network.setReceiveAction (Receiver);
+			machines = new Configuration (machinesINI);
+
+		}				
+
+/*		public notificationServerReceiver ()
+		{
+			config = new monotifications.configuration ("server.ini");
+			network.listenPort = int.Parse (config ["server"] ["port"]);			
+			network.setReceiveAction (receiver);			
+		}*/
+		
+		
+		
+		public void Receiver (string content)
+		{
+			if (content.StartsWith ("<")) {	
+				Message msg = new Message ();
+				msg.parse (content);
+				
+				if (msg ["type"] == "-1")
+					switch (msg ["content"]) {
+					case "regiser":
+					case "keep-alive":
+						RegisterClient (msg ["myAddress"], msg ["myPort"], msg ["subscription"]);
+						break;
+					}
+				Console.WriteLine ("XML dump: {0}", content);
+				//Console.WriteLine (string.Format ("New message receivd: {0}", msg ["content"]));
+			} else
+				Console.WriteLine (string.Format ("New text message: {0}", content));	
+		}
+		
+		public void RegisterClient (string addr, string port, string grp)
+		{
+			machines [addr] ["address"] = addr;
+			machines [addr] ["port"] = port;
+			machines [addr] ["grp"] = grp;
+			machines [addr] ["lastseen"] = DateTime.Now.ToString ();
+			//machines.TriggerSave ();
+			machines.Save ();
+		}
+		
+		public static void __Main (string[] args)
+		{
+			NotificationServer server = new NotificationServer ("server.ini", 7778);
+			server.startListener ();
+			server.config.TriggerSave();
+						
+		}
+	}
+}
