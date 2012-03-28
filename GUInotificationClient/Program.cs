3e310a17d30b0ba8ad3f2a3b0bb4e3b6ae1f1c24
Tomer Cohen <tomerc+github.com@gmail.com>
2012-03-28 16:25:36 +0200

    GUI agent

---
+++ GUInotificationClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GUInotificationClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void _Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
