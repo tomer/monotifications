f83eb13b18ad8c16153fd66bf53ff26f198f355b
Tomer Cohen <tomerc+github.com@gmail.com>
2012-04-05 02:14:03 +0300

    Updating Assemblies

--- NotificationServerGUI/Properties/AssemblyInfo.cs
+++ NotificationServerGUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotificationServerGUI")]
+[assembly: AssemblyTitle("Notification Server GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kinneret")]
-[assembly: AssemblyProduct("NotificationServerGUI")]
-[assembly: AssemblyCopyright("Copyright © Kinneret 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("moNotification")]
+[assembly: AssemblyCopyright("Copyright © Tomer Cohen 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
